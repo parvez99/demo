3a829fe1d6e21aec47b4e9753c7af2d35b05241f
parvez.mulani <parvez.mulani@coinmetrics.io>
2025-08-15 02:46:03 +0200

    [circleci] adding dummy file for test to work

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,8 @@
-# tests/test_api.py
 import os
 os.environ["DATABASE_URL"] = "sqlite+pysqlite:///:memory:"  # DB-less tests
 
 from fastapi.testclient import TestClient
-from pmulaniapi.main import app   # import from your package
+from pmulaniapi.main import app
 
 client = TestClient(app)
 
